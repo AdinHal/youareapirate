b2123145fc52a52545379723c58dc5be71f852e6
AdinHal <80127944+AdinHal@users.noreply.github.com>
2024-07-07 21:41:44 +0200

    Smaller text and combat modification, hitboxes reduced for british soliders

--- Script/Sword.gd
+++ Script/Sword.gd
@@ -9,10 +9,10 @@ func _on_body_entered(body):
 	for child in body.get_children():
 		if child is Damageable:
 			child.hit(damage-5)
-			print_debug(body.name + " took "+str(damage)+".")
+			#print_debug(body.name + " took "+str(damage)+".")
 		elif child is Damage:
 			child.hit(damage)
-			print_debug(body.name + " took "+str(damage)+".")
+			#print_debug(body.name + " took "+str(damage)+".")
 		elif child is BritishDamage:
 			child.hit(damage-10)
-			print_debug(body.name + " took "+str(damage)+".")
+			#print_debug(body.name + " took "+str(damage)+".")
